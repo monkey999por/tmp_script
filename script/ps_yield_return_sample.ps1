2843baa7e6ebb337bbb61748891b47f2c336bb55
monkey999por <yururi1122@gmail.com>
2020-09-13 02:08:06 +0900

    tet

---
+++ script/ps_yield_return_sample.ps1
@@ -0,0 +1,25 @@
+﻿#hello world
+
+class MyClass {
+
+    [string]$name = "satoshi"
+
+    [int]$age = 20
+
+    [System.Collections.ArrayList[string]]$favolite
+
+    #[System.Collections.Generic.Dictionary[string, string]]$mapping
+
+    MyClass (){
+        #$this.favolite = [System.Collections.ArrayList[string]]::new()
+    }
+
+    [MyClass] getField () {
+        return [MyClass]::new()
+    }
+}
+
+$result = [MyClass]::new()
+$result.getField()
+
+
